ea2375a7dfb7ec5cb42f46e086020da7c796f2af
DigitalCowboy3000 <jonas.dubey@gmail.com>
2026-01-28 14:44:51 +0100

    Update Prototype.h

--- C_Lib/Prototype.h
+++ C_Lib/Prototype.h
@@ -1,1 +1,30 @@
+/*------------------------------------------------------------------------
+Prototype.h
+Library exemple 
+
+uc: STM32 G071RBT6
+
+Création		: -
+Révision		: -
+
+Description:Fonctions de Gestion du bus UART
+--------------------------------------------------------------------------*/
+#ifndef _PROTOTYPE_H_
+#define _PROTOTYPE_H_
+
+// Equivalences
+
+// Variables globales
+
+// Prototypes de fonctions (déclarations)
+
+extern void **********(void);
+/*------------------------------------------------------------------------
+Fonction  :
+Description  :
+Entrées  :  -
+Sorties  :  -
+--------------------------------------------------------------------------*/
+
+#endif 
 
