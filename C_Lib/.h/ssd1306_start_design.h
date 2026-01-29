002ad68ab2930ddb17910992d6f817af60ba7e74
Jonas Dubey <jonas.dubey@gmail.com>
2026-01-29 10:22:45 +0100

    Update ssd1306_start_design.h

--- C_Lib/.h/ssd1306_start_design.h
+++ C_Lib/.h/ssd1306_start_design.h
@@ -1,1 +1,36 @@
+/*------------------------------------------------------------------------
+Prototype.h
+Library exemple 
 
+uc: STM32 G071RBT6
+
+Création		: -
+Révision		: -
+
+Description  :
+--------------------------------------------------------------------------*/
+#ifndef _SSD1306_START_DESIGN_H_
+#define _SSD1306_START_DESIGN_H_
+
+// Equivalences
+
+// Variables globales
+
+// Prototypes de fonctions (déclarations)
+
+extern void ssd1306_start_fill_cube(void);
+/*------------------------------------------------------------------------
+Fonction  :
+Description  :
+Entrées  :  -
+Sorties  :  -
+--------------------------------------------------------------------------*/
+extern void ssd1306_start_contrast_waiting(void);
+/*------------------------------------------------------------------------
+Fonction  :
+Description  :
+Entrées  :  -
+Sorties  :  -
+--------------------------------------------------------------------------*/
+
+#endif 
